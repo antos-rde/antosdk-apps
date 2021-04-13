3c2e622b3ad76a48109853bdd0cf3af3ddf87d43
mrsang <mrsang@iohub.dev>
2021-04-13 20:03:53 +0200

    add monaco editor core library

---
+++ Antedit/coffees/main.coffee
@@ -0,0 +1,7 @@
+class Antedit extends this.OS.application.BaseApplication
+    constructor: ( args ) ->
+        super "Antedit", args
+    
+    main: () ->
+
+this.OS.register "Antedit", Antedit
---
+++ Antedit/ts/main.ts
@@ -0,0 +1,19 @@
+
+namespace OS {
+
+    export namespace application {
+        /**
+         *
+         * @class Antedit
+         * @extends {BaseApplication}
+         */
+        export class Antedit extends BaseApplication {
+            constructor(args: AppArgumentsType[]) {
+                super("Antedit", args);
+            }
+            main(): void {
+                // YOUR CODE HERE
+            }
+        }
+    }
+}
